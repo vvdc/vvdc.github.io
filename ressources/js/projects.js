b882d2051713be3ad604a54b010eb190ff16beb2
vvdc <81557398+vvdc@users.noreply.github.com>
2023-06-27 00:31:30 +0200

    Add files via upload

--- ressources/js/projects.js
+++ ressources/js/projects.js
@@ -20,13 +20,8 @@ const itemModal8= document.querySelector('#modal--portfolio .item8');
 const btnBalises = document.querySelectorAll('.container--cards .card--project a');
 //affichage item portfolio
 const closeBtnPortfolio= document.querySelector('.closebtnportfolio');
-
-
-//fermeture de la modale
-closeBtnPortfolio.addEventListener('click', ()=>{
-    modalPortfolio.style.left="-250%";
-});
-
+// recuperation de la position de la section portfolio
+const positionSectionPorfolio= document.querySelector("#portfolio").offsetHeight;
 
 
 // gestion de l'affichage de la modal portfolio en fonction du btn cliqué sur les cartes portfolio
@@ -50,7 +45,19 @@ function Action(){
                 modalDescription.innerText= data['project'][idInt].description;
                 modalImg1.src=data['project'][idInt]['src-image1'];
                 modalImg2.src=data['project'][idInt]['src-image2'];
-                modalLink.href=data['project'][idInt]['btn-link'];
+                //gestion du lien inactif
+                // si le l'anchor ne comporte pas de lien
+                if(modalLink.href=data['project'][idInt]['btn-link'] == ""){
+                    //modification de l'apparence du lien 
+                    modalLink.innerText= "non disponible";
+                    modalLink.style.background= "rgba(240, 102, 102)";
+                    //on annule le comportement du lien
+                    modalLink.addEventListener('click', (e)=>{
+                        e.preventDefault();
+                    })
+                }else{
+                    modalLink.href=data['project'][idInt]['btn-link'];
+                }
                 itemModal1.innerText=data['project'][idInt]['points-clés'][0].item1;
                 itemModal2.innerText=data['project'][idInt]['points-clés'][0].item2;
                 itemModal3.innerText=data['project'][idInt]['points-clés'][0].item3;
@@ -62,19 +69,18 @@ function Action(){
 
                 //affichage de la modal
                 modalPortfolio.style.left="50%";
-
-                //gestion du lien inactif
-                // si le l'anchor ne comporte pas de lien
-                if(modalLink.href=data['project'][idInt]['btn-link'] == ""){
-                    //modification de l'apparence du lien 
-                    modalLink.innerText= "lein non disponible";
-                    modalLink.style.background= "rgba(240, 102, 102)";
-                    //on annule le comportement du lien
-                    modalLink.addEventListener('click', (e)=>{
-                        e.preventDefault();
-                    })
-                }
+                //empecher le scroll du body
+                document.body.style.position="fixed";
             })
         })
     }
 }
+
+//fermeture de la modale
+closeBtnPortfolio.addEventListener('click', ()=>{
+    modalPortfolio.style.left="-250%";
+    document.body.style.position="relative";
+    window.scroll(0, positionSectionPorfolio);
+});
+
+
